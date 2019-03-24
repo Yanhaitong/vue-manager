3f690aaf85cfa980ea7082272cad1395b004f4ca
yht <930237972@qq.com>
2019-03-24 21:49:11 +0800

    2

--- src/views/nav2/ProductAdvertising.vue
+++ src/views/nav2/ProductAdvertising.vue
@@ -49,9 +49,9 @@
                 </el-form-item>
                 <el-form-item label="广告位置" prop="type" @change="typeChange">
                     <el-select v-model="addForm.type" placeholder="请选择广告位置">
-                        <el-option label="启动图" value="0"></el-option>
-                        <el-option label="首页弹框" value="1"></el-option>
-                        <el-option label="首页轮播图" value="2"></el-option>
+                        <el-option label="启动图" value="0">启动图</el-option>
+                        <el-option label="首页弹框" value="1">首页弹框</el-option>
+                        <el-option label="首页轮播图" value="2">首页轮播图</el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="客户端" prop="client" @change="clientChange">
@@ -76,9 +76,9 @@
                 </el-form-item>
                 <el-form-item label="广告位置" prop="type">
                     <el-select v-model="editForm.type" placeholder="请选择广告位置">
-                        <el-option label="启动图" value="0">启动图</el-option>
-                        <el-option label="首页弹框" value="1">首页弹框</el-option>
-                        <el-option label="首页轮播图" value="2">首页轮播图</el-option>
+                        <el-option label="启动图" key="0" value="0">启动图</el-option>
+                        <el-option label="首页弹框" key="1" value="1">首页弹框</el-option>
+                        <el-option label="首页轮播图" key="2" value="2">首页轮播图</el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="客户端" prop="client" @change="clientChange">
@@ -123,7 +123,7 @@
                 addLoading: false,
                 editFormVisible: false,
                 editForm: {
-                    typeName: ''
+                    type: ''
                 },
                 editLoading: false,
 
@@ -239,8 +239,8 @@
             //显示编辑界面
             handleEdit: function (index, row) {
                 this.editFormVisible = true;
+                debugger
                 this.editForm = Object.assign({}, row);
-                this.editForm.type = 1;
             },
 
             //显示新增界面
@@ -248,17 +248,6 @@
                 this.addFormVisible = true;
             },
 
-            selsChange: function (sels) {
-                this.sels = sels;
-            },
-
-            clientChange: function (sels) {
-                this.clientName = sels;
-            },
-
-            typeChange: function (sels) {
-                this.type = sels;
-            },
         },
         mounted() {
             this.getProductAdvertisingList();
